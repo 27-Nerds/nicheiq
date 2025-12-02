9ace63920ba0a196b28d21ee5a26d71a3b71fdc6
syzspectroom <syzspectroom@gmail.com>
2025-12-02 18:33:03 +0200

    landing page

---
+++ landing-page/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>NicheIQ - Validated Market Research in 12 Minutes</title>
+	<link rel="icon" href="/favicon.svg" />
+</svelte:head>
+
+{@render children()}
